cbd1ae37f7ca0a1234139072632a2e32703a91c1
thainguyenvann <thaitancang001@gmail.com>
2018-09-14 10:41:56 +0700

    finish sinhvien_class

---
+++ src/Thai/dev/SinhVien.java
@@ -0,0 +1,17 @@
+package Thai.dev;
+
+public class SinhVien {
+
+	private String name;
+	private double Toan;
+	private double Ly;
+	private double Hoa;
+		
+	public SinhVien(String name, double Toan, double Ly, double Hoa) {	
+		
+	this.name = name;
+	this.Toan = Toan;
+	this.Ly = Ly;
+	this.Hoa = Hoa;
+	}
+}
